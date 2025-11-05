a44ac953e254c7310613c763bcd8f94e5e5a9f11
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-05 21:36:10 +0530

    Update gitbuild.groovy

--- vars/gitbuild.groovy
+++ vars/gitbuild.groovy
@@ -2,7 +2,7 @@ def call() {
     stage('Git Checkout') {
         // use 'script' level step directly
         checkout([$class: 'GitSCM',
-            branches: [[name: '*/master']],
+            branches: [[name: '*/development']],
             userRemoteConfigs: [[url: 'https://github.com/Tudumu-0509/maven-webapplication-project-kkfunda.git']]
         ])
     }
