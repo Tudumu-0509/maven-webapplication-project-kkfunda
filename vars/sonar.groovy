acc8df5a5a52d7cf823e9662441e65dd0144f7b0
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-06 00:34:34 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -2,11 +2,10 @@ def call() {
     echo "Running SonarQube Analysis..."
     withSonarQubeEnv('MySonarQube') {
         withCredentials([string(credentialsId: 'SONAR_TOKEN_ID', variable: 'SONAR_TOKEN')]) {
-            dir('maven-webapplication-project-kkfunda') {
                 sh "mvn sonar:sonar -Dsonar.login=${SONAR_TOKEN}"
             }
         }
     }
-}
+
 
 
