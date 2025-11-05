d44fcd80a403e7b739a637f25fc08d1f4f1f52bc
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-06 00:17:13 +0530

    Update gitapp.groovy

--- vars/gitapp.groovy
+++ vars/gitapp.groovy
@@ -1,6 +1,12 @@
 def call() {
     stage('Build') {
         echo "Building the application..."
+
+        // Step 1: Clone the Git repository
+        git branch: 'development', url: 'https://github.com/kkfunda/maven-webapplication-project-kkfunda.git'
+
+        // Step 2: Build the Maven project
         sh 'mvn clean package'
     }
 }
+
