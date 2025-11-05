7fbdb7795bbfbb32cedd5aa29e9a21c23329ef06
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-05 20:04:49 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,16 +1,9 @@
-def call() {
-    stage('SonarQube Analysis') {
-        echo "🔍 Running SonarQube analysis..."
-        withCredentials([string(credentialsId: 'sonarqube-token', variable: 'SONAR_TOKEN')]) {
-            withSonarQubeEnv('MySonarServer') {
-                sh """
-                    mvn clean verify sonar:sonar \
-                      -Dsonar.login=${SONAR_TOKEN}
-                """
-            }
-        }
-    }
+def call(){
+stage('sonar report'){
+	steps{
+	sh "mvn sonar:sonar"
+	}
 }
-
+}
 
 
