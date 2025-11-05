93bb88bb2761d1f0360476b052fb1c0dcf21b42a
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-05 22:05:28 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,9 +1,15 @@
-// vars/sonar.groovy
-def call() {
-	stage('soanr')
-    // Run the SonarQube scan
-    sh "mvn sonar:sonar"
+def call(String projectKey, String sonarHostUrl) {
+    // Use Jenkins credentials securely
+    withCredentials([string(credentialsId: 'SONAR_TOKEN', variable: 'SONAR_TOKEN')]) {
+        sh """
+            mvn clean install sonar:sonar \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.host.url=${sonarHostUrl} \
+            -Dsonar.login=$SONAR_TOKEN
+        """
+    }
 }
+
 
 
 
