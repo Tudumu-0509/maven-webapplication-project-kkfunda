031a8672717c720f56d76e547cdac4a24a403d9d
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-06 01:00:06 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,17 +1,17 @@
 def call() {
     echo "Running SonarQube Analysis..."
-
     withSonarQubeEnv('MySonarQube') {
         withCredentials([string(credentialsId: 'SONAR_TOKEN_ID', variable: 'SONAR_TOKEN')]) {
-            sh '''
+            sh """
                 mvn sonar:sonar \
                 -Dsonar.projectKey=maven-web-application-kkfunda \
                 -Dsonar.host.url=http://3.236.159.117:9000 \
-                -Dsonar.login=$SONAR_TOKEN
-            '''
+                -Dsonar.login=${SONAR_TOKEN}
+            """
         }
     }
 }
+
 
 
 
