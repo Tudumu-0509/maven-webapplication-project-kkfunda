097d4cfa5f0cd60e849f6cc4b5326ad1483c2916
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-09 00:15:44 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,1 +1,10 @@
-
+def call() {
+    stage('Deploy to Tomcat') {
+        echo "Deploying to Tomcat..."
+        sh """
+            curl -u manesha:password \\
+            --upload-file target/maven-web-application.war \\
+            "http://3.235.1.233:8080/manager/text/deploy?path=/maven-web-application&update=true"
+        """
+    }
+}
