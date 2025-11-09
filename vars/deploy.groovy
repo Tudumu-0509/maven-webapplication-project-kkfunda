9f2779a62de006d850abe26a57aaf809b9c30ced
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-09 15:44:43 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,11 +1,24 @@
-def call() {
+def call(Map config = [:]) {
+
     stage('Deploy to Tomcat') {
-        echo "Deploying to Tomcat..."
+        // Default values (you can override if needed)
+        def user = config.user ?: "manesha"
+        def password = config.password ?: "password"
+        def warFile = config.war ?: "target/maven-web-application.war"
+        def tomcatUrl = config.tomcatUrl ?: "http://3.238.142.178:8080/manager/text/deploy?path=/maven-web-application&update=true"
+
+        echo "🚀 Deploying ${warFile} to Tomcat..."
+        echo "Tomcat URL: ${tomcatUrl}"
+
+        // Run curl deployment
         sh """
-            curl -u manesha:password \\
-            --upload-file target/maven-web-application.war \\
-            "http://3.238.142.178:8080/manager/text/deploy?path=/maven-web-application&update=true"
+            curl -u ${user}:${password} \
+            --upload-file ${warFile} \
+            "${tomcatUrl}"
         """
+        
+        echo "✅ Deployment completed successfully!"
     }
 }
+
 
