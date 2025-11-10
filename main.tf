c45b093f05170fb42dc8a4479a9775e81315bf33
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-10 21:13:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,16 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"          
-  access_key = "YOUR_AWS_ACCESS_KEY"
-  secret_key = "YOUR_AWS_SECRET_KEY"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-069e612f612be3a2b"  
-  instance_type = "t3.micro"               
+  ami           = "ami-069e612f612be3a2b"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform-EC2"
   }
 }
+
