e55fdf1b33706c7fb4d1bdea11b0a556557a23b7
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-10 23:59:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,15 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-069e612f612be3a2b"
-  instance_type = "t3.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   tags = {
-    Name = "Terraform-EC2"
+    Name = var.instance_name
   }
 }
 
