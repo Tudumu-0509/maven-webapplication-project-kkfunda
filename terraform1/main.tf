9cf5c5d223aea02fdae5591301dda5aaf26b0e7e
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-15 13:26:57 +0530

    Update main.tf

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -11,49 +11,14 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Security Group – HTTP(8080) & SSH(22) open
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP and SSH"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# EC2 Instance
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name   # Key pair create చేసి
+  key_name      = var.key_name
 
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  
 
   tags = {
     Name = var.instance_name
   }
-
- 
-}
-
-# Output – EC2 Public IP
-output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
 }
